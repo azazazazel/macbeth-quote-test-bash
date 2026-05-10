8d5b0eb6eee772650380574e954edd90bae6801d
Athena <athenacrawley@gmail.com>
2026-05-10 12:55:24 +0100

    updated passgen and README

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ macbeth key quote tester, now in bash!
 
 # fullup
 extremely simple apt update + upgrade + autoremove script (to save me like 30 keystrokes)
+
+# passgen
+password generator based on a sha256 checksum of the current time
--- passgen.sh
+++ passgen.sh
@@ -1,4 +1,13 @@
 #! /bin/sh
 # passgen, written by azazazazel
 
-date +%s%N | cksum -a sha256 | cut -b 14-29
+length=15
+
+while getopts "l:" arg; do
+  case $arg in
+    l) length=$OPTARG;;
+  esac
+done
+
+length=$(( $length + 13 ))
+date +%s%N | cksum -a sha256 | cut -b 14-$length
